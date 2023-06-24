24021b0c1017f27cdbd0e6fdcaab9304b30ca965
John Robert M. Mamauag <137342511+jrmamauag007@users.noreply.github.com>
2023-06-24 09:57:00 +0800

    Add files via upload

--- ssisv2back.py
+++ ssisv2back.py
@@ -69,13 +69,34 @@ def view_courses():
     conn.close()
     return rows
 
-def search(name="",id="",sex="",year="",code=""):
+def search(name=None, id=None, sex=None, year=None, code=None):
     conn = sqlite3.connect("Students.db")
     cur = conn.cursor()
-    cur.execute("Select * FROM students WHERE name=? or id=? or sex=? or year=? or code=?",(name,id,sex,year,code))
-    rows = cur.fetchall()
+
+    query = "SELECT * FROM students WHERE 1=1"
+
+    if name:
+        query += f" AND name = '{name}'"
+
+    if id:
+        query += f" AND id = '{id}'"
+
+    if sex:
+        query += f" AND sex = '{sex}'"
+
+    if year:
+        query += f" AND year = '{year}'"
+
+    if code:
+        query += f" AND code = '{code}'"
+
+    cur.execute(query)
+    results = cur.fetchall()
+
     conn.close()
-    return(rows)
+
+    return results
+
 
 def delete_student(num):
 
@@ -84,33 +105,112 @@ def delete_student(num):
     cur.execute("DELETE FROM students WHERE num=?",(num,))
     conn.commit()
     conn.close()
+    
 def delete_course(num):
-
-    conn = sqlite3.connect("Students.db")
-    cur  = conn.cursor()
-    cur.execute("DELETE FROM courses WHERE num=?",(num,))
-    conn.commit()
-    conn.close()
-
-def update_students(num,name,id,sex,year,code):
-    if not all((num,name,id,sex,year,code)):
-        messagebox.showerror("Error", "Student fields must be filled.")
-        return
     conn = sqlite3.connect("Students.db")
     cur = conn.cursor()
+
+    # Retrieve the course details before deleting
+    cur.execute("SELECT code, course FROM courses WHERE num=?", (num,))
+    result = cur.fetchone()
+
+    if result is None:
+        messagebox.showerror("Error", "Course not found.")
+        conn.close()
+        return
+
+    code = result[0]
+    course = result[1]
+
+    # Double confirmation
+    confirmation = messagebox.askyesno("Confirmation", f"Are you sure you want to delete the course: {course}?\n"
+                                                      f"All associated students with the old course will also be deleted.")
+
+
+    if confirmation:
+        # Delete the course
+        cur.execute("DELETE FROM courses WHERE num=?", (num,))
+        conn.commit()
+        cur.execute("DELETE FROM students WHERE code=?", (code,))
+        conn.commit()
+        conn.close()
+        messagebox.showinfo("Success", "Course deleted successfully.")
+    else:
+        messagebox.showinfo("Deletion Canceled", "Deletion operation canceled.")
+
+
+def update_students(num,name,id,sex,year,code):
+
+    conn = sqlite3.connect("Students.db")
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM courses WHERE code = ?", (code,))
+    result = cur.fetchone()
+    if result is None:
+        messagebox.showerror("Error", f"Course '{code}' does not exist")
+        conn.close()
+
+    cur.execute("SELECT * FROM students WHERE num=?", (num,))
+    result = cur.fetchone()
+
+
+    stored_id = result[2]  # Get the stored id from the fetched result
+
+    if id != stored_id:
+        # Check if the new id is different from the stored id
+        cur.execute("SELECT * FROM students WHERE id=?", (id,))
+        result = cur.fetchone()
+        if result is not None:
+            messagebox.showerror("Error", f"Student with ID '{id}' already exists")
+            conn.close()
+            return
+        
     cur.execute("UPDATE students SET name=?, id=?, sex=?, year=?,code=? WHERE num=?",(name,id,sex,year,code,num))
     conn.commit()
     conn.close()
 
-def update_courses(num,name,course):
-    if not all((num,num,name,course)):
+def update_courses(num, code, course):
+    if not all((num, code, course)):
         messagebox.showerror("Error", "Course fields must be filled.")
         return
+
     conn = sqlite3.connect("Students.db")
     cur = conn.cursor()
-    cur.execute("UPDATE courses SET code=?, course=? WHERE num=?",(name,course,num))
+
+    # Retrieve the old course details before updating
+    cur.execute("SELECT code, course FROM courses WHERE num=?", (num,))
+    result = cur.fetchone()
+
+    if result is None:
+        messagebox.showerror("Error", "Course not found.")
+        conn.close()
+        return
+
+    old_code = result[0]
+    old_course = result[1]
+
+    # Double confirmation
+    confirmation = messagebox.askyesno("Confirmation", f"Are you sure you want to update the course: {old_code} - {old_course}?\n"
+                                                      f"All associated students with the old course will also be updated.")
+
+    if confirmation:
+        # Update the course details
+        cur.execute("UPDATE courses SET code=?, course=? WHERE num=?", (code, course, num))
+        conn.commit()
+
+        # Update the course code for associated students
+        cur.execute("UPDATE students SET code=? WHERE code=?", (code, old_code))
+
+        conn.commit()
+        conn.close()
+        messagebox.showinfo("Success", "Course and associated students updated successfully.")
+    else:
+        messagebox.showinfo("Update Canceled", "Update operation canceled.")
+
+
     conn.commit()
     conn.close()
+
 
 def delete_data():
     
--- ssisv2front.py
+++ ssisv2front.py
@@ -5,55 +5,69 @@ import ssisv2back
 from tkinter import messagebox
 
 def get_selected_row(event):
-    ''' get_selected_row function to get content of the selected row.
-    
-        Arguments
-        ---------
-        event: a virtual interrupt.
-    '''
     global selected_tuple
-    if lb1.curselection() != ():
- 
-        index = lb1.curselection()[0]
-        selected_tuple = lb1.get(index)
+    if tree1.selection():
+
+        item = tree1.selection()[0]
+        selected_tuple = tree1.item(item)['values']
         clear_entries()
+        e1.delete(0, END)
         e1.insert(END, selected_tuple[1])
+        e2.delete(0, END)
         e2.insert(END, selected_tuple[2])
-        sex_value = selected_tuple[3]
-        sex_index = sex_options.index(sex_value)
+        sex_value = selected_tuple[2]
+        sex_index = sex_options.index(sex_value) if sex_value in sex_options else 0
         sex_combo.current(sex_index)
-        year_value = selected_tuple[4]
-        year_index = year_options.index(year_value)
+        year_value = selected_tuple[3]
+        year_index = year_options.index(year_value) if year_value in year_options else 0
         year_combo.current(year_index)
+        e3.delete(0, END)
         e3.insert(END, selected_tuple[5])
-        
+        selected_tuple = None
+
+    if tree2.selection():
+        item = tree2.selection()[0]
+        selected_tuple = tree2.item(item)['values']
+        clear_entries()
+        e3.delete(0, END)
+        e3.insert(END, selected_tuple[1])
+        e4.delete(0, END)
+        e4.insert(END, selected_tuple[2])
+        selected_tuple = None
 
 
 def viewstudents_command():
     ''' view_command function to show the output database.
     
     '''
-    lb1.delete(0, END)  
+    tree1.delete(*tree1.get_children())  # Clear the treeview
     for row in ssisv2back.view_students():
-        lb1.insert(END, row)
+        tree1.insert('', END, values=row)
+    
 
 def viewcourses_command():
     ''' view_command function to show the output database.
     
     '''
-    lb1.delete(0, END)  
+    tree2.delete(*tree2.get_children())  # Clear the treeview
     for row in ssisv2back.view_courses():
-        lb1.insert(END, row)
+        tree2.insert('', END, values=row)
+    
 
 
 def search_command():
-    ''' search_command function to search in the database.
-    
-    '''
-    lb1.delete(0, END)
-    for row in ssisv2back.search(name.get(), id.get(), sex.get(), year.get(),code.get()):
-        lb1.insert(END, row)
-    clear_entries()
+    tree1.delete(*tree1.get_children())  # Clear the existing items in the tree1 widget
+
+
+    students = ssisv2back.search(name.get(), id.get(), sex.get(), year.get(), code.get())
+
+    if not students:
+        messagebox.showinfo("No Results", "No matching students found.")
+        return
+
+    for student in students:
+        tree1.insert("", "end", values=student)
+
 
 
 def addstudent_command():
@@ -72,27 +86,34 @@ def addcourse_command():
 
 def updatestudent_command():
     global selected_tuple
-    if not lb1.curselection():
+    if not tree1.selection():
         messagebox.showwarning("Warning", "No student selected.")
         return
+    item = tree1.selection()[0]
+    selected_tuple = tree1.item(item)['values']
+    if selected_tuple is None:
+        messagebox.showwarning("Warning", "No student data selected.")
+        return
 
-    # Prompt for user confirmation
     confirmation = messagebox.askyesno("Confirmation", "Are you sure you want to update the student's data?")
-
     if confirmation:
-        ssisv2back.update_students(selected_tuple[0], name.get(), id.get(), sex.get(), year.get(),code.get())
+        if not name.get() or not id.get() or not sex.get() or not year.get() or not code.get():
+            messagebox.showwarning("Warning", "Please fill in all fields.")
+            return
+        item = tree1.selection()[0]
+        selected_tuple = tree1.item(item)['values']
+        ssisv2back.update_students(selected_tuple[0],name.get(), id.get(), sex.get(), year.get(), code.get())
+        messagebox.showinfo("Success", "Student data updated successfully.")
         clear_entries()
         viewstudents_command()
     else:
         messagebox.showinfo("Update Canceled", "Update operation canceled.")
 
 
+
 def updatecourse_command():
-    ''' updatecourse_command function to update the data of a specific course.
-    
-    '''
     global selected_tuple
-    if not lb1.curselection():
+    if not tree2.selection():
         messagebox.showwarning("Warning", "No course selected.")
         return
 
@@ -100,19 +121,20 @@ def updatecourse_command():
     confirmation = messagebox.askyesno("Confirmation", "Are you sure you want to update the course's data?")
 
     if confirmation:
-        ssisv2back.update_courses(selected_tuple[0], code.get(),course.get())
+        item = tree2.selection()[0]
+        selected_tuple = tree2.item(item)['values']
+        ssisv2back.update_courses(selected_tuple[0], code.get(), course.get())
         clear_entries()
         viewcourses_command()
     else:
         messagebox.showinfo("Update Canceled", "Update operation canceled.")
-
     
 
 def deletestudent_command():
     ''' deletestudent_command function to delete the data of a specific student.
     
     '''
-    if not lb1.curselection():
+    if not tree1.selection():
         messagebox.showwarning("Warning", "No student selected.")
         return
 
@@ -120,8 +142,8 @@ def deletestudent_command():
     confirmation = messagebox.askyesno("Confirmation", "Are you sure you want to delete the student's data?")
 
     if confirmation:
-        index = lb1.curselection()[0]
-        selected_tuple = lb1.get(index)
+        item = tree1.selection()[0]
+        selected_tuple = tree1.item(item)['values']
         ssisv2back.delete_student(selected_tuple[0])
         clear_entries()
         viewstudents_command()
@@ -133,7 +155,7 @@ def deletecourse_command():
     ''' deletecourse_command function to delete the data of a specific course.
     
     '''
-    if not lb1.curselection():
+    if not tree2.selection():
         messagebox.showwarning("Warning", "No course selected.")
         return
 
@@ -141,14 +163,14 @@ def deletecourse_command():
     confirmation = messagebox.askyesno("Confirmation", "Are you sure you want to delete the course's data?")
 
     if confirmation:
-        index = lb1.curselection()[0]
-        selected_tuple = lb1.get(index)
+        item = tree2.selection()[0]
+        selected_tuple = tree2.item(item)['values']
         ssisv2back.delete_course(selected_tuple[0])
         clear_entries()
         viewcourses_command()
     else:
         messagebox
-
+    
 
 
 def delete_data_command():
@@ -171,15 +193,25 @@ def clear_entries():
     '''
     e1.delete(0, END)
     e2.delete(0, END)
+    sex_combo.set('') 
+    year_combo.set('')
     e3.delete(0, END)
     e4.delete(0, END)
 
+def clearall():
+    clear_entries()
+    tree1.selection_remove(tree1.selection())
+    tree2.selection_remove(tree2.selection())
+    global selected_tuple
+    selected_tuple = None
 
+    
 def clear_command():
     ''' view_command function to clear content of Listbox.
     
     '''
-    lb1.delete(0, END)
+    tree1.delete(0, END)
+    tree2.delete(0, END)
     clear_entries()
 
 # Create the main window
@@ -207,20 +239,20 @@ year_options = ['Freshman', 'Sophomore', 'Junior', 'Senior']
 year_combo = ttk.Combobox(window, textvariable=year, values=year_options, state='readonly')
 
 # Create labels for entry fields
-ttk.Label(window, text="Name:").grid(row=0, column=0, padx=5, pady=5)
-ttk.Label(window, text="ID:").grid(row=1, column=0, padx=5, pady=5)
-ttk.Label(window, text="Sex:").grid(row=2, column=0, padx=5, pady=5)
-ttk.Label(window, text="Year:").grid(row=3, column=0, padx=5, pady=5)
-ttk.Label(window, text="Course Code:").grid(row=4, column=0, padx=5, pady=5)
-ttk.Label(window, text="Course Name:").grid(row=4, column=2, padx=5, pady=5)
+ttk.Label(window, text="Name:").grid(row=0, column=2, padx=5, pady=5)
+ttk.Label(window, text="ID:").grid(row=1, column=2, padx=5, pady=5)
+ttk.Label(window, text="Sex:").grid(row=2, column=2, padx=5, pady=5)
+ttk.Label(window, text="Year:").grid(row=3, column=2, padx=5, pady=5)
+ttk.Label(window, text="Course Code:").grid(row=4, column=2, padx=5, pady=5)
+ttk.Label(window, text="Course Name:").grid(row=4, column=4, padx=5, pady=5)
 
 # Position entry fields
-e1.grid(row=0, column=1, padx=5, pady=5)
-e2.grid(row=1, column=1, padx=5, pady=5)
-sex_combo.grid(row=2, column=1, padx=5, pady=5)
-year_combo.grid(row=3, column=1, padx=5, pady=5)
-e3.grid(row=4, column=1, padx=5, pady=5)
-e4.grid(row=4, column=3, padx=5, pady=5)
+e1.grid(row=0, column=3, padx=5, pady=5)
+e2.grid(row=1, column=3, padx=5, pady=5)
+sex_combo.grid(row=2, column=3, padx=5, pady=5)
+year_combo.grid(row=3, column=3, padx=5, pady=5)
+e3.grid(row=4, column=3, padx=5, pady=5)
+e4.grid(row=4, column=5, padx=5, pady=5)
 
 # Create buttons
 view_students_button = ttk.Button(window, text="View Students", command=viewstudents_command)
@@ -232,35 +264,75 @@ update_course_button = ttk.Button(window, text="Update Course", command=updateco
 delete_student_button = ttk.Button(window, text="Delete Student", command=deletestudent_command)
 delete_course_button = ttk.Button(window, text="Delete Course", command=deletecourse_command)
 search_button = ttk.Button(window, text="Search", command=search_command)
-clear_button = ttk.Button(window, text="Clear", command=clear_entries)
+clear_button = ttk.Button(window, text="Clear", command=clearall)
 delete_all_button = ttk.Button(window, text="Delete All", command=delete_data_command)
 exit_button = ttk.Button(window, text="Exit", command=clear_command)
 
 # Position buttons
-view_students_button.grid(row=0, column=2, padx=5, pady=5)
-view_courses_button.grid(row=1, column=2, padx=5, pady=5)
-add_student_button.grid(row=0, column=3, padx=5, pady=5)
-add_course_button.grid(row=1, column=3, padx=5, pady=5)
-update_student_button.grid(row=2, column=2, padx=5, pady=5)
-update_course_button.grid(row=3, column=2, padx=5, pady=5)
-delete_student_button.grid(row=2, column=3, padx=5, pady=5)
-delete_course_button.grid(row=3, column=3, padx=5, pady=5)
-search_button.grid(row=6, column=0, padx=5, pady=5)
-clear_button.grid(row=6, column=1, padx=5, pady=5)
-delete_all_button.grid(row=6, column=2, padx=5, pady=5)
-exit_button.grid(row=6, column=3, padx=5, pady=5)
+view_students_button.grid(row=0, column=4, padx=5, pady=5)
+view_courses_button.grid(row=0, column=5, padx=5, pady=5)
+add_student_button.grid(row=1, column=4, padx=5, pady=5)
+add_course_button.grid(row=1, column=5, padx=5, pady=5)
+update_student_button.grid(row=2, column=4, padx=5, pady=5)
+update_course_button.grid(row=2, column=5, padx=5, pady=5)
+delete_student_button.grid(row=3, column=4, padx=5, pady=5)
+delete_course_button.grid(row=3, column=5, padx=5, pady=5)
+search_button.grid(row=6, column=2, padx=5, pady=5)
+clear_button.grid(row=6, column=3, padx=5, pady=5)
+delete_all_button.grid(row=6, column=4, padx=5, pady=5)
+exit_button.grid(row=6, column=5, padx=5, pady=5)
 
 
-# Create listbox and scrollbar
-lb1 = tk.Listbox(window, height=10, width=70)
-lb1.bind('<<ListboxSelect>>',get_selected_row)
-lb1.grid(row=8, column=0, columnspan=4, rowspan=10, padx=5, pady=5)
+# Create Treeview for Database 1
+tree1 = ttk.Treeview(window, height=10, columns=('Index','Name', 'ID', 'Sex', 'Year Level', 'Course'), selectmode='extended')
+tree1.bind('<<TreeviewSelect>>', get_selected_row)
+tree1['show'] = 'headings'
+tree1.grid(row=8, column=0, columnspan=4, padx=5, pady=5,sticky="nsew")
 
-sc = ttk.Scrollbar(window, command=lb1.yview)
-sc.grid(row=8, column=5, rowspan=10, sticky='ns')
+# Create scrollbar for Treeview 1
+sc1 = ttk.Scrollbar(window, command=tree1.yview)
+sc1.grid(row=8, column=4, sticky='ns')
 
-lb1.configure(yscrollcommand=sc.set)
+# Configure scrollbar for Treeview 1
+tree1.configure(yscrollcommand=sc1.set)
 
+# Add columns to Treeview 1
+tree1.heading('Name', text='Name')
+tree1.heading('ID', text='ID')
+tree1.heading('Sex', text='Sex')
+tree1.heading('Year Level', text='Year Level')
+tree1.heading('Course', text='Course')
+
+# Set column widths for Treeview 1
+tree1.column("Index", width=10)
+tree1.column('Name', width=150, minwidth=50, anchor='w')
+tree1.column('ID', width=100, minwidth=50, anchor='w')
+tree1.column('Sex', width=100, minwidth=50, anchor='w')
+tree1.column('Year Level', width=100, minwidth=50, anchor='w')
+tree1.column('Course', width=100, minwidth=50, anchor='w')
+
+
+# Create Treeview for Database 2
+tree2 = ttk.Treeview(window, height=10, columns=('Index','Course Code', 'Course Name'), selectmode='extended')
+tree2.bind('<<TreeviewSelect>>', get_selected_row)
+tree2['show'] = 'headings'
+tree2.grid(row=8, column=5, columnspan=2, padx=5, pady=5,sticky="nsew")
+
+# Create scrollbar for Treeview 2
+sc2 = ttk.Scrollbar(window, command=tree2.yview)
+sc2.grid(row=8, column=8, sticky='ns')
+
+# Configure scrollbar for Treeview 2
+tree2.configure(yscrollcommand=sc2.set)
+
+# Add columns to Treeview 2
+tree2.heading('Course Code', text='Course Code')
+tree2.heading('Course Name', text='Course Name')
+
+# Set column widths for Treeview 2
+tree2.column("Index", width=10)
+tree2.column('Course Code', width=150, minwidth=50, anchor='w')
+tree2.column('Course Name', width=150, minwidth=50, anchor='w')
 # Run the main event loop
 window.mainloop()
 
